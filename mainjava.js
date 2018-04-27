a483f4ac0cdb895f80d4b8e295d0c0422a93579d
AliTawbe <33085291+AliTawbe@users.noreply.github.com>
2018-04-27 06:52:12 +0300

    Add files via upload

--- mainjava.js
+++ mainjava.js
@@ -1,18 +1,18 @@
-
-var menu = ('.menu');
-var barra_busqueda = ('.search_bar');
-var bar = ('#bar');
-var icono = ('.icon-search');
-barra_busqueda.focusin(function(){
-  barra_busqueda.css('width', '20%');
-  menu.css('width', '80%');
-  icono.css('color', '#000');
-  bar.css('color', '#000');
-});
-barra_busqueda.focusout(function(){
-  barra_busqueda.css('width', '15%');
-  menu.css('width', '85%');
-  icono.css('color', '#fff');
-  bar.css('color', '#fff');
-});
-
+// Add no-touch class to body for mobile touch events and toggle hover class on elements that need it
+	if ("ontouchstart" in document.documentElement) {
+		document.documentElement.className += " touch";
+	}
+	
+	// Add and remove no-hover class to <li>'s for mobile hover events
+	jQuery('.touch .container').each(function() {
+		var div = jQuery(this);
+		
+		div.hover(function() {
+			div.removeClass('no-hover');
+		});
+		
+		jQuery('*').not(div).bind('click', function() {
+			div.addClass('no-hover');
+		});
+		
+	});
